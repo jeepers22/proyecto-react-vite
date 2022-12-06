a637bb0470b428f9ab1370f7f2c46364f2012ab6
Maximiliano Safran <maxizero@gmail.com>
2022-12-06 02:50:08 -0300

    Migrando Bootstrap por ReactBootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from "./components/NavBar"
 import ItemListContainer from "./components/ItemListContainer"
 import ItemDetailContainer from "./components/ItemDatailContainer"
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,25 +1,30 @@
 import CartWidget from "./CartWidget"
 import "./NavBar.css"
+import Navbar from "react-bootstrap/Navbar"
+import Container from "react-bootstrap/Container"
+import Nav from 'react-bootstrap/Nav';
 import { NavLink } from "react-router-dom"
 
 const NavBar = () => {
     return (
-        <nav className="navbar fixed-top navbar-expand-md navbar-dark bg-dark navegador">
-            <div className="container d-flex justify-content-around">
-                <NavLink to={"/"} className="navbar-brand logo" >MovieHouse</NavLink>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse d-flex-column justify-content-md-end align-items-center gap-xl-5" id="navbarNavAltMarkup">
-                    <div className="navbar-nav gap-md-3 text-center">
-                        <NavLink to={"/genero/1"} className="nav-link" href="#">Comedia</NavLink>
-                        <NavLink to={"/genero/2"} className="nav-link" href="#">Drama</NavLink>
-                        <NavLink to={"/genero/3"} className="nav-link" href="#">Terror</NavLink>
-                    </div>
-                    <a className="logo-carrito" href="#"><CartWidget /></a>
-                </div>
-            </div>
-        </nav>
+        <>
+            <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
+                <Container>
+                    <NavLink to={"/"} className="navbar-brand logo" >MovieHouse</NavLink>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <NavLink to={"/genero/1"} className="nav-link" href="#">Comedia</NavLink>
+                            <NavLink to={"/genero/2"} className="nav-link" href="#">Drama</NavLink>
+                            <NavLink to={"/genero/3"} className="nav-link" href="#">Terror</NavLink>
+                        </Nav>
+                        <Nav>
+                            <a className="logo-carrito" href="#"><CartWidget /></a>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
     )
 }
 
