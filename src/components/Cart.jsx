051da76d098e13dc85f1b472e18a3a22264bfc15
Maximiliano Safran <maxizero@gmail.com>
2022-12-20 14:33:55 -0300

    Feature: agregado productos a carrito y suma repetidos

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { CartContext } from './CartContext'
+import { useContext } from 'react'
 
 const Cart = () => {
+
+    const { cartItems } = useContext(CartContext)
+
     return (
-        <p>Hola soy el carrito</p>
+        <ul>
+            {
+                cartItems.length
+                // Si hay items en el carrito
+                ? cartItems.map(item => <li key={item.idPelicula}>{item.titulo} - {item.qty}</li>)
+                // Si tengo 0 items
+                : <p>El carrito se encuentra vacío</p>
+            }
+        </ul>
     )
 }
 export default Cart
