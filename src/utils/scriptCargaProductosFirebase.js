aeaedab9ef1a52582cf99ce84e2b72bc7ff5d010
Maximiliano Safran <maxizero@gmail.com>
2022-12-31 17:12:52 -0300

    Eliminamos comentarios sin utilidad para la entrega

--- src/utils/scriptCargaProductosFirebase.js
+++ src/utils/scriptCargaProductosFirebase.js
@@ -18,7 +18,7 @@ const catalogo = [
         categoria: "Album",
         nombre: "Naruto Shippuden",
         anio: 2021,
-        precio: 4000,
+        precio: 1500,
         stock: 3,
         descripcion: "Albun Naruto Shippuden",
         portada: "https://res.cloudinary.com/datqk3byv/image/upload/v1672152282/album-naruto_rmmjqd.png"
@@ -28,8 +28,8 @@ const catalogo = [
         categoria: "Figuritas",
         nombre: "Messi",
         anio: 2022,
-        precio: 5500,
-        stock: 15,
+        precio: 3000,
+        stock: 5,
         descripcion: "Figurita de Lionel Messi (GOAT), capitán de la selección argentina",
         portada: "https://res.cloudinary.com/datqk3byv/image/upload/v1672152281/figu-messi_y7mduw.jpg"
     },
@@ -38,8 +38,8 @@ const catalogo = [
         categoria: "Figuritas",
         nombre: "Cristiano Ronaldo",
         anio: 2022,
-        precio: 3300,
-        stock: 1,
+        precio: 2500,
+        stock: 3,
         descripcion: "Figurita de Cristiano Ronaldo (CR7), capitán de la selección de Portugal",
         portada: "https://res.cloudinary.com/datqk3byv/image/upload/v1672152281/figu-cr7_xjopch.jpg"
     },
@@ -48,8 +48,8 @@ const catalogo = [
         categoria: "Figuritas",
         nombre: "Qatar 2022",
         anio: 2022,
-        precio: 2800,
-        stock: 5,
+        precio: 300,
+        stock: 55,
         descripcion: "Figuritas Qatar 2022 en paquete sellado",
         portada: "https://res.cloudinary.com/datqk3byv/image/upload/v1672152281/figus-mundial_izlruc.png"
     },
@@ -58,8 +58,8 @@ const catalogo = [
         categoria: "Figuritas",
         nombre: "Naruto 2021",
         anio: 2021,
-        precio: 1500,
-        stock: 8,
+        precio: 150,
+        stock: 30,
         descripcion: "Figuritas Naruto 2021 en paquete sellado",
         portada: "https://res.cloudinary.com/datqk3byv/image/upload/v1672152282/figus-naruto_jsvcag.png"
     },
@@ -68,8 +68,8 @@ const catalogo = [
         categoria: "Coleccionables",
         nombre: "Emboscada de Veloceleste",
         anio: 2004,
-        precio: 1500,
-        stock: 8,
+        precio: 7000,
+        stock: 4,
         descripcion: "Emboscada de Veloceleste, una de las cartas mas valiosas del momento",
         portada: "https://res.cloudinary.com/datqk3byv/image/upload/v1672152282/magic-2_f0pcn4.jpg"
     },
@@ -78,8 +78,8 @@ const catalogo = [
         categoria: "Coleccionables",
         nombre: "Desdén de Sharkhan",
         anio: 2004,
-        precio: 1500,
-        stock: 8,
+        precio: 8500,
+        stock: 3,
         descripcion: "Desdén de Sharkhan, una de las cartas más dificiles de conseguir",
         portada: "https://res.cloudinary.com/datqk3byv/image/upload/v1672152282/magic-1_rw7meu.jpg"
     },
@@ -88,8 +88,8 @@ const catalogo = [
         categoria: "Coleccionables",
         nombre: "Stampede Dwarf",
         anio: 2004,
-        precio: 1500,
-        stock: 8,
+        precio: 8000,
+        stock: 1,
         descripcion: "Stampede Dwarf, carta importada, hay sólo unas pocas unidades en el mundo",
         portada: "https://res.cloudinary.com/datqk3byv/image/upload/v1672152282/magic-3_ovojug.jpg"
     },
@@ -98,8 +98,8 @@ const catalogo = [
         categoria: "Coleccionables",
         nombre: "Slinn Voda",
         anio: 2004,
-        precio: 1500,
-        stock: 8,
+        precio: 4300,
+        stock: 6,
         descripcion: "Slinn Voda, el Abisal Emergente. Estupenda bestia marina con un asombroso poder",
         portada: "https://res.cloudinary.com/datqk3byv/image/upload/v1672152282/magic-4_bjw8g5.jpg"
     }
@@ -107,21 +107,12 @@ const catalogo = [
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-// Creado con maxizero@gmail.com se me excedió la cuota diaria
     apiKey: "AIzaSyCL1EuNUhGtuTk9CYJC-ZYUpTc1RQjbZVg",
     authDomain: "proyecto-react-e75cf.firebaseapp.com",
     projectId: "proyecto-react-e75cf",
     storageBucket: "proyecto-react-e75cf.appspot.com",
     messagingSenderId: "130925470865",
     appId: "1:130925470865:web:82933498e0ab0a38c9cb30"
-
-    // Creado con maxizerodev@gmail.com
-    // apiKey: "AIzaSyD85SGU5RHT6m9hcoZuzpVN7Nsnhwdjslw",
-    // authDomain: "proyecto-final-react-b5e77.firebaseapp.com",
-    // projectId: "proyecto-final-react-b5e77",
-    // storageBucket: "proyecto-final-react-b5e77.appspot.com",
-    // messagingSenderId: "374680553485",
-    // appId: "1:374680553485:web:252700d13f9dd3107f2d44"
 };
 
 // Initialize Firebase
