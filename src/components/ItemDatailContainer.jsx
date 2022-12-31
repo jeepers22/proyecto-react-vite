fa8d39fd8ce883bd60d13c7f16b22df1b6a54ded
Maximiliano Safran <maxizero@gmail.com>
2022-12-31 14:08:17 -0300

    Script separado para importar documento desde FireStore

--- src/components/ItemDatailContainer.jsx
+++ src/components/ItemDatailContainer.jsx
@@ -2,8 +2,7 @@ import ItemDetail from "./ItemDetail"
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import Container from "react-bootstrap/Container"
-import { doc, getDoc } from "firebase/firestore"
-import { db } from "../utils/firebaseConfig"
+import { getDocFromFirebase } from "../utils/ABMFirebase"
 
 const ItemDatailContainer = () => {
 
@@ -13,19 +12,7 @@ const ItemDatailContainer = () => {
 
 	// ComponentDidUpdate
 	useEffect(() => {
-		const getDocFromFirebase = async () => {
-			const docRef = doc(db, "products", idProdParam)
-            const docProduct = await getDoc(docRef)
-			if (docProduct.exists()) {
-				return {
-					idProd: docProduct.id,
-					...docProduct.data()
-				}
-			} else {
-				console.log("Intentando acceder a un Item inexistente")
-			}
-        }
-        getDocFromFirebase()
+        getDocFromFirebase(idProdParam)
             .then (result => setprodEstado(result))
             .catch (err => console.log(err))
 	}, [idProdParam])
--- src/utils/ABMFirebase.js
+++ src/utils/ABMFirebase.js
@@ -1,9 +1,10 @@
-import { collection, getDocs, where, query } from "firebase/firestore"
+import { collection, getDocs, getDoc, doc, where, query } from "firebase/firestore"
 import { db } from "../utils/firebaseConfig"
 
 export const getCollectionFromFirebase = async (idCategoriaParam) => {
     let q
     if (idCategoriaParam) {
+        // where(atributo colección, "==", id traído por parámetro)
         q = query(collection(db, "products"), where("idCategoria", "==", parseInt(idCategoriaParam)))
     } else {
         q = query(collection(db, "products"))
@@ -15,4 +16,17 @@ export const getCollectionFromFirebase = async (idCategoriaParam) => {
             ...doc.data()
         }))
     return products
+}
+
+export const getDocFromFirebase = async (idProdParam) => {
+    const docRef = doc(db, "products", idProdParam)
+    const docProduct = await getDoc(docRef)
+    if (docProduct.exists()) {
+        return {
+            idProd: docProduct.id,
+            ...docProduct.data()
+        }
+    } else {
+        console.log("Intentando acceder a un Item inexistente")
+    }
 }
